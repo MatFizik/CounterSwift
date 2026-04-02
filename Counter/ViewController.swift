1b9237d41363cf29d09da2773e79abcef3a8a0c6
Adilkhan <dfw.adilhan@gmail.com>
2026-04-02 12:10:04 +0600

    кнопки + и -, история, счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    var currentCount: Int = 0
+    
+    var history: [String] = []
+    
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "dd MMM HH:mm:ss"
     }
 
-
+    
+    @IBAction func plusButtonDidTap() {
+        currentCount += 1
+        countLabel.text = "\(currentCount)"
+        
+        history.append("[\(dateFormatter.string(from: Date()))]: значение изменено на +1")
+        historyTextView.text = history.joined(separator: "\n")
+        
+    }
+    
+    
+    @IBAction func minusButtonDidTap() {
+        currentCount -= 1
+        countLabel.text = "\(currentCount)"
+        
+        history.append("[\(dateFormatter.string(from: Date()))]: значение изменено на -1")
+        historyTextView.text = history.joined(separator: "\n")
+        
+    }
 }
 
