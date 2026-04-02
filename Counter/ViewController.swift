491c9c87acb9245aa868683848b3d51d52d9da78
Adilkhan <dfw.adilhan@gmail.com>
2026-04-02 12:53:15 +0600

    сброс счётчика, вставка в начало истории

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var trashButton: UIButton!
     
     @IBOutlet weak var historyTextView: UITextView!
     
@@ -34,19 +35,34 @@ class ViewController: UIViewController {
         currentCount += 1
         countLabel.text = "\(currentCount)"
         
-        history.append("[\(dateFormatter.string(from: Date()))]: значение изменено на +1")
+        history.insert("[\(dateFormatter.string(from: Date()))]: значение изменено на +1", at: 0)
         historyTextView.text = history.joined(separator: "\n")
         
     }
     
     
     @IBAction func minusButtonDidTap() {
-        currentCount -= 1
-        countLabel.text = "\(currentCount)"
+        if(currentCount < 1){
+            history.insert("[\(dateFormatter.string(from: Date()))]: попытка уменьшить значение счётчика ниже 0", at: 0)
+        } else {
+            currentCount -= 1
+            countLabel.text = "\(currentCount)"
+            
+            history.insert("[\(dateFormatter.string(from: Date()))]: значение изменено на -1", at: 0)
+        }
         
-        history.append("[\(dateFormatter.string(from: Date()))]: значение изменено на -1")
+        
         historyTextView.text = history.joined(separator: "\n")
         
+    }
+    
+    @IBAction func trashButtonDidTap() {
+        if(currentCount > 0){
+            currentCount = 0
+            countLabel.text = "\(currentCount)"
+            history.insert("[\(dateFormatter.string(from: Date()))]: значение сброшено", at: 0)
+            historyTextView.text = history.joined(separator: "\n")
+        }
     }
 }
 
